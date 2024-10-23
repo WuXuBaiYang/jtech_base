99e20f14f64feb4414dcc3704ba33f42508fe016
wuxubaiyang <wuxubaiyang@gmail.com>
2024-10-23 10:21:33 +0800

    新增模板中的接口请求部分示例;

--- templete/lib/common/common.dart
+++ templete/lib/common/common.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 /*
 * 静态资源/通用静态变量
 * @author wuxubaiyang
@@ -6,4 +8,13 @@
 class Common {
   // 数据库名称
   static const String databaseName = '{jtech_base_db_name}.db';
+
+  // 请求基本地址
+  static const String baseUrl = kDebugMode ? devUrl : prodUrl;
+
+  // 开发地址
+  static const String devUrl = '{jtech_base_dev_url}';
+
+  // 生产地址
+  static const String prodUrl = '{jtech_base_prod_url}';
 }
