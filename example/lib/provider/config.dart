db6a4462866dca15429788053d89d4a8d90aeba1
戊戌白羊 <wuxubaiyang@gmail.com>
2025-06-13 15:39:16 +0800

    +1

--- example/lib/provider/config.dart
+++ example/lib/provider/config.dart
@@ -1,4 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:jtech_base/jtech_base.dart';
+
+part 'config.g.dart';
+
+part 'config.freezed.dart';
 
 /*
 * 全局设置
@@ -7,58 +12,34 @@ import 'package:jtech_base/jtech_base.dart';
 */
 class ConfigProvider extends BaseConfigProvider<AppConfig> {
   ConfigProvider(super.context)
-      : super(creator: AppConfig.from, serializer: (c) => c.to());
+    : super(
+        creator: (e) => AppConfig.fromJson(e),
+        serializer: (c) => c.toJson(),
+      );
 
   // 开关状态
-  bool get switcher => config.switcher;
+  bool get switcher => config.switcher ?? false;
 
   // 更新开关状态
   void updateSwitcher(bool switcher) =>
       updateConfig(config.copyWith(switcher: switcher));
 
   // 进度
-  double get progress => config.progress;
+  double get progress => config.progress ?? 0;
 
   // 更新进度
   void updateProgress(double progress) =>
       updateConfig(config.copyWith(progress: progress));
 }
 
-/*
-* 配置文件对象
-* @author wuxubaiyang
-* @Time 2024/8/14 14:40
-*/
-class AppConfig {
-  // 开关
-  final bool switcher;
+// 配置对象
+@freezed
+abstract class AppConfig with _$AppConfig {
+  const factory AppConfig({
+    required bool? switcher,
+    required double? progress,
+  }) = _AppConfig;
 
-  // 进度
-  final double progress;
-
-  AppConfig({
-    required this.switcher,
-    required this.progress,
-  });
-
-  AppConfig.from(dynamic obj)
-
-      /// 可以在这里设置默认值
-      : switcher = obj['switcher'] ?? false,
-        progress = obj['progress'] ?? 0.5;
-
-  Map<String, dynamic> to() => {
-        'switcher': switcher,
-        'progress': progress,
-      };
-
-  AppConfig copyWith({
-    bool? switcher,
-    double? progress,
-  }) {
-    return AppConfig(
-      switcher: switcher ?? this.switcher,
-      progress: progress ?? this.progress,
-    );
-  }
+  factory AppConfig.fromJson(Map<String, dynamic> json) =>
+      _$AppConfigFromJson(json);
 }
